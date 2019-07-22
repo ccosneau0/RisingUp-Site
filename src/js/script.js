592f936ed06eaa72cc302ceb6d44608fb7d0ac7a
Charles Cosneau <charles.cosneau@hetic.net>
2019-07-22 17:44:57 +0200

    changes on presentiel.html/css cause to add animations

---
+++ src/js/script.js
@@ -0,0 +1,35 @@
+// Selection des éléments gu groupe innovation
+const innovationHover = document.getElementById('innovation__hover');
+const innovation = document.getElementById('innovation');
+const innovationPicture = document.getElementById('innovation__picture');
+const innovationTitle = document.getElementById('innovation__title');
+const innovationSummary = document.getElementById('innovation__summary');
+const innovationList = document.getElementById('innovation__list');
+const innovationButton = document.getElementById('innovation__button');
+const innovationCatchphrase = document.getElementById('innovation__catchphrase');
+const innovationNumber = document.getElementById('innovation__number');
+
+
+
+innovationHover.addEventListener('mouseover', function() {
+  innovation.classList.replace('innovation', 'innovation--hover');
+  innovationPicture.classList.replace('innovation__picture', 'innovation__picture--hover');
+  innovationCatchphrase.classList.replace('innovation__catchphrase', 'innovation__catchphrase--hover');
+  innovationTitle.classList.replace('innovation__title', 'innovation__title--hover');
+  innovation__summary.classList.replace('innovation__summary', 'innovation__summary--hover');
+  innovationNumber.classList.replace('innovation__number', 'innovation__number--hover');
+  innovationList.classList.replace('innovation__list', 'innovation__list--hover');
+  innovationButton.classList.replace('innovation__button', 'innovation__button--hover');
+
+});
+
+innovationHover.addEventListener('mouseleave', function() {
+  innovation.classList.replace('innovation--hover', 'innovation');
+  innovationPicture.classList.replace('innovation__picture--hover', 'innovation__picture');
+  innovationTitle.classList.replace('innovation__title--hover', 'innovation__title');
+  innovationCatchphrase.classList.replace('innovation__catchphrase--hover', 'innovation__catchphrase');
+  innovation__summary.classList.replace('innovation__summary--hover', 'innovation__summary');
+  innovationNumber.classList.replace('innovation__number--hover', 'innovation__number');
+  innovationList.classList.replace('innovation__list--hover', 'innovation__list');
+  innovationButton.classList.replace('innovation__button--hover', 'innovation__button');
+});
