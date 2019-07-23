0f196f1e1f40cd836e0a36647dbb6d2c96ae1c8a
Charles Cosneau <charles.cosneau@hetic.net>
2019-07-23 18:05:21 +0200

    animations

--- src/js/script.js
+++ src/js/script.js
@@ -8,28 +8,37 @@ const innovationList = document.getElementById('innovation__list');
 const innovationButton = document.getElementById('innovation__button');
 const innovationCatchphrase = document.getElementById('innovation__catchphrase');
 const innovationNumber = document.getElementById('innovation__number');
-
+let isHover = false;
 
 // Animation au hover de la section presentiel
 innovationHover.addEventListener('mouseover', function() {
-  innovation.classList.replace('innovation', 'innovation--hover');
-  innovationPicture.classList.replace('innovation__picture', 'innovation__picture--hover');
-  innovationCatchphrase.classList.replace('innovation__catchphrase', 'innovation__catchphrase--hover');
-  innovationTitle.classList.replace('innovation__title', 'innovation__title--hover');
-  innovation__summary.classList.replace('innovation__summary', 'innovation__summary--hover');
-  innovationNumber.classList.replace('innovation__number', 'innovation__number--hover');
-  innovationList.classList.replace('innovation__list', 'innovation__list--hover');
-  innovationButton.classList.replace('innovation__button', 'innovation__button--hover');
+  if(isHover === false) {
+    isHover = true;
+    innovation.classList.replace('innovation', 'innovation--hover');
+    innovationPicture.classList.replace('innovation__picture', 'innovation__picture--hover');
+    innovationCatchphrase.classList.replace('innovation__catchphrase', 'innovation__catchphrase--hover');
+    innovationTitle.classList.replace('innovation__title', 'innovation__title--hover');
+    innovation__summary.classList.replace('innovation__summary', 'innovation__summary--hover');
+    innovationNumber.classList.replace('innovation__number', 'innovation__number--hover');
+    innovationList.classList.replace('innovation__list', 'innovation__list--hover');
+    innovationButton.classList.replace('innovation__button', 'innovation__button--hover');
+    console.log('mouseover');
+  }
 
 });
 
 innovationHover.addEventListener('mouseleave', function() {
-  innovation.classList.replace('innovation--hover', 'innovation');
-  innovationPicture.classList.replace('innovation__picture--hover', 'innovation__picture');
-  innovationTitle.classList.replace('innovation__title--hover', 'innovation__title');
-  innovationCatchphrase.classList.replace('innovation__catchphrase--hover', 'innovation__catchphrase');
-  innovation__summary.classList.replace('innovation__summary--hover', 'innovation__summary');
-  innovationNumber.classList.replace('innovation__number--hover', 'innovation__number');
-  innovationList.classList.replace('innovation__list--hover', 'innovation__list');
-  innovationButton.classList.replace('innovation__button--hover', 'innovation__button');
+  if(isHover === true) {
+    isHover = false;
+    innovation.classList.replace('innovation--hover', 'innovation');
+    innovationPicture.classList.replace('innovation__picture--hover', 'innovation__picture');
+    innovationTitle.classList.replace('innovation__title--hover', 'innovation__title');
+    innovationCatchphrase.classList.replace('innovation__catchphrase--hover', 'innovation__catchphrase');
+    innovation__summary.classList.replace('innovation__summary--hover', 'innovation__summary');
+    innovationNumber.classList.replace('innovation__number--hover', 'innovation__number');
+    innovationList.classList.replace('innovation__list--hover', 'innovation__list');
+    innovationButton.classList.replace('innovation__button--hover', 'innovation__button');
+    console.log('mouseleave');
+  }
+
 });
