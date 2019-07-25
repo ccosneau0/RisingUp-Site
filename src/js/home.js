ad06c3b05aa2e70c28ee44ad08c7271cf1485f03
Charles Cosneau <charles.cosneau@hetic.net>
2019-07-25 13:20:48 +0200

    rework home.html / presentiel.html

---
+++ src/js/home.js
@@ -0,0 +1,12 @@
+// var homeStart = document.getElementsByClassName('home__start');
+// console.log(homeStart);
+//
+// function getScrollY() {
+//   var scrollY = window.scrollY;
+//   if(scrollY > 2416) {
+//     homeStart.style.backgroundColor = "#26385B";
+//     console.log(scrollY);
+//   }
+// }
+//
+// setInterval(getScrollY, 300);
