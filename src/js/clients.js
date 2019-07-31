8a0fbd861ad815618aa6abb53d1a67d67511ff3c
Charles Cosneau <charles.cosneau@hetic.net>
2019-07-31 16:44:09 +0200

    parallax on clients.html

--- src/js/clients.js
+++ src/js/clients.js
@@ -1,1 +1,196 @@
 var rellax = new Rellax('.client__bubble');
+
+//SNCF
+const bubbleSncf = document.querySelector('.client__bubble--sncf');
+const infoSncf = document.querySelector('.bubble__info--sncf');
+
+//BAMBIN
+const bubbleBambin = document.querySelector('.client__bubble--bambin');
+const infoBambin = document.querySelector('.bubble__info--bambin');
+
+//BAYARD
+const bubbleBayard = document.querySelector('.client__bubble--bayard');
+const infoBayard = document.querySelector('.bubble__info--bayard');
+
+//MICHELIN
+const bubbleMichelin = document.querySelector('.client__bubble--michelin');
+const infoMichelin = document.querySelector('.bubble__info--michelin');
+
+//SOLID
+const bubbleSolid = document.querySelector('.client__bubble--solid');
+const infoSolid = document.querySelector('.bubble__info--solid');
+
+//EXTIA
+const bubbleExtia = document.querySelector('.client__bubble--extia');
+const infoExtia = document.querySelector('.bubble__info--extia');
+
+//HETIC
+const bubbleHetic = document.querySelector('.client__bubble--hetic');
+const infoHetic = document.querySelector('.bubble__info--hetic');
+
+//MAIRIE
+const bubbleMairie = document.querySelector('.client__bubble--mairie');
+const infoMairie = document.querySelector('.bubble__info--mairie');
+
+//LEARN
+const bubbleLearn = document.querySelector('.client__bubble--learn');
+const infoLearn = document.querySelector('.bubble__info--learn');
+
+//KIDS
+const bubbleKids = document.querySelector('.client__bubble--kids');
+const infoKids = document.querySelector('.bubble__info--kids');
+
+//CFPC
+const bubbleCfpc = document.querySelector('.client__bubble--cfpc');
+const infoCfpc = document.querySelector('.bubble__info--cfpc');
+
+//IESA
+const bubbleIesa = document.querySelector('.client__bubble--iesa');
+const infoIesa = document.querySelector('.bubble__info--iesa');
+
+//RF
+const bubbleRf = document.querySelector('.client__bubble--rf');
+const infoRf = document.querySelector('.bubble__info--rf');
+
+//DEC
+const bubbleDec = document.querySelector('.client__bubble--dec');
+const infoDec = document.querySelector('.bubble__info--dec');
+
+//LYON
+const bubbleLyon = document.querySelector('.client__bubble--lyon');
+const infoLyon = document.querySelector('.bubble__info--lyon');
+
+// SNCF
+bubbleSncf.addEventListener('mouseover', function() {
+  infoSncf.style.display = 'inline-block';
+})
+
+bubbleSncf.addEventListener('mouseleave', function() {
+  infoSncf.style.display = 'none';
+})
+
+//BAMBIN
+bubbleBambin.addEventListener('mouseover', function() {
+  infoBambin.style.display = 'inline-block';
+})
+
+bubbleBambin.addEventListener('mouseleave', function() {
+  infoBambin.style.display = 'none';
+})
+
+//BAYARD
+bubbleBayard.addEventListener('mouseover', function() {
+  infoBayard.style.display = 'inline-block';
+})
+
+bubbleBayard.addEventListener('mouseleave', function() {
+  infoBayard.style.display = 'none';
+})
+
+//MICHELIN
+bubbleMichelin.addEventListener('mouseover', function() {
+  infoMichelin.style.display = 'inline-block';
+})
+
+bubbleMichelin.addEventListener('mouseleave', function() {
+  infoMichelin.style.display = 'none';
+})
+
+//SOLID
+bubbleSolid.addEventListener('mouseover', function() {
+  infoSolid.style.display = 'inline-block';
+})
+
+bubbleSolid.addEventListener('mouseleave', function() {
+  infoSolid.style.display = 'none';
+})
+
+//EXTIA
+bubbleExtia.addEventListener('mouseover', function() {
+  infoExtia.style.display = 'inline-block';
+})
+
+bubbleExtia.addEventListener('mouseleave', function() {
+  infoExtia.style.display = 'none';
+})
+
+//HETIC
+bubbleHetic.addEventListener('mouseover', function() {
+  infoHetic.style.display = 'inline-block';
+})
+
+bubbleHetic.addEventListener('mouseleave', function() {
+  infoHetic.style.display = 'none';
+})
+
+//MAIRIE
+bubbleMairie.addEventListener('mouseover', function() {
+  infoMairie.style.display = 'inline-block';
+})
+
+bubbleMairie.addEventListener('mouseleave', function() {
+  infoMairie.style.display = 'none';
+})
+
+//LEARN
+bubbleLearn.addEventListener('mouseover', function() {
+  infoLearn.style.display = 'inline-block';
+})
+
+bubbleLearn.addEventListener('mouseleave', function() {
+  infoLearn.style.display = 'none';
+})
+
+//KIDS
+bubbleKids.addEventListener('mouseover', function() {
+  infoKids.style.display = 'inline-block';
+})
+
+bubbleKids.addEventListener('mouseleave', function() {
+  infoKids.style.display = 'none';
+})
+
+//CFPC
+bubbleCfpc.addEventListener('mouseover', function() {
+  infoCfpc.style.display = 'inline-block';
+})
+
+bubbleCfpc.addEventListener('mouseleave', function() {
+  infoCfpc.style.display = 'none';
+})
+
+//IESA
+bubbleIesa.addEventListener('mouseover', function() {
+  infoIesa.style.display = 'inline-block';
+})
+
+bubbleIesa.addEventListener('mouseleave', function() {
+  infoIesa.style.display = 'none';
+})
+
+//RF
+bubbleRf.addEventListener('mouseover', function() {
+  infoRf.style.display = 'inline-block';
+})
+
+bubbleRf.addEventListener('mouseleave', function() {
+  infoRf.style.display = 'none';
+})
+
+//DEC
+bubbleDec.addEventListener('mouseover', function() {
+  infoDec.style.display = 'inline-block';
+})
+
+bubbleDec.addEventListener('mouseleave', function() {
+  infoDec.style.display = 'none';
+})
+
+//LYON
+bubbleLyon.addEventListener('mouseover', function() {
+  infoLyon.style.display = 'inline-block';
+})
+
+bubbleLyon.addEventListener('mouseleave', function() {
+  infoLyon.style.display = 'none';
+})
